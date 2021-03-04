8319a03396f9bd9f9a88197e9a64f4ff781169a6
utkarsh0124 <52751458+utkarsh0124@users.noreply.github.com>
2021-03-04 10:06:17 +0530

    Add files via upload

---
+++ JNEXT.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main(){
+	fastIO
+	int tc;
+	cin>>tc;
+	while(tc--){
+		int n;
+		cin>>n;
+		int arr[n];
+		for(int i=0; i<n; i++) cin>>arr[i];
+		
+		//solution
+		if(n==1) cout<<-1<<'\n';
+		else{
+			int index = -1;
+			for(int i=n-2; i>-1; i--){
+				if(arr[i]<arr[i+1]){
+					index =i;
+					break;
+				}
+			}
+			if(index<0) cout<<-1<<'\n';
+			else{
+				stack<int> s1,s2;
+				int i=index+1;
+				
+				while(i<n){
+					if(arr[i]<=arr[index]) break;
+					s1.push(arr[i]);
+					i++;
+				}
+				while(i<n){
+					if(arr[i]<=arr[index]) s2.push(arr[i]);
+					i++;
+				}
+				
+				int tmp = arr[index];
+				arr[index] = s1.top();
+				s1.pop();
+				s1.push(tmp);
+				
+				while(!s2.empty()){
+					arr[index+1]=s2.top();
+					s2.pop();
+					index++;
+				}
+				while(!s1.empty()){
+					arr[index+1]=s1.top();
+					s1.pop();
+					index++;
+				}
+				for(int j:arr) cout<<j;
+				cout<<'\n';
+			}
+		}		
+		
+	}
+	return 0;
+}
